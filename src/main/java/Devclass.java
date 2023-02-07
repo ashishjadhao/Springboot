ac6360a9b7cfce024c8e317a400b99e4e9b8f649
Ashish <jadhaoashish26gmail.com>
2023-02-07 14:56:36 +0530

    commit to Final

--- src/main/java/Devclass.java
+++ src/main/java/Devclass.java
@@ -2,5 +2,6 @@ public class Devclass {
     public static void main(String[] args) {
         System.out.println("Devclass");
         System.out.println("Check");
+        System.out.println("Final");
     }
 }
